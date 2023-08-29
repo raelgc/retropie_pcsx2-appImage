0676ee48ee00be9b6efef3a784c0c221b89e9e5d
Rael Gugelmin Cunha <rael.gc@gmail.com>
2023-08-29 10:58:58 -0300

    Renaming installer to reflect this uses the AppImage

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # PCSX2 Qt Setup for Retropie
 
-Retropie plugin to install and update the [PCSX2 emulator](https://pcsx2.net) with the new [Qt interface](https://www.reddit.com/r/emulation/comments/uvf7cd/pcsx2_first_public_qt_release_is_now_available/) (AVX2 version).
+Retropie plugin to install and update the [PCSX2 emulator](https://pcsx2.net) with the new [Qt interface](https://www.reddit.com/r/emulation/comments/uvf7cd/pcsx2_first_public_qt_release_is_now_available/) using the [AppImage]() version, which requires no compilation.
 
-This will add an entry on [Retropie-Setup](https://github.com/RetroPie/RetroPie-Setup) named `pcsx2-qt`.
+This will add an entry on [Retropie-Setup](https://github.com/RetroPie/RetroPie-Setup) named `pcsx2-appImage`.
 
 Available only on `X86` (aka PC) platform.
 
@@ -24,10 +24,10 @@ ps2_fullname="PlayStation 2"
 Install the `pcsx2-qt` pluging running in a terminal:
 
 ```bash
-wget https://raw.githubusercontent.com/raelgc/retropie_pcsx2-qt/master/pcsx2-qt.sh -O ~/RetroPie-Setup/scriptmodules/emulators/pcsx2-qt.sh
+wget https://raw.githubusercontent.com/raelgc/retropie_pcsx2-qt/master/pcsx2-appImage.sh -O ~/RetroPie-Setup/scriptmodules/emulators/pcsx2-appImage.sh
 ```
 
-Now you can run **RetroPie Setup** script and `rpcs2-qt` will available under `exp` (experimental) packages section.
+Now you can run **RetroPie Setup** script and `rpcs2-appImage` will available under `exp` (experimental) packages section.
 
 ## Playing
 
--- pcsx2-appImage.sh
+++ pcsx2-appImage.sh
@@ -9,25 +9,25 @@
 # at https://raw.githubusercontent.com/RetroPie/RetroPie-Setup/master/LICENSE.md
 #
 
-rp_module_id="pcsx2-qt"
-rp_module_desc="PS2 emulator PCSX2 - New Qt frontend (AVX2)"
+rp_module_id="pcsx2-appImage"
+rp_module_desc="PS2 emulator PCSX2 - AppImage"
 rp_module_help="ROM Extensions: .bin .iso .img .mdf .z .z2 .bz2 .cso .chd .ima .gz\n\nCopy your PS2 roms to $romdir/ps2\n\nCopy the required BIOS file to $biosdir"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/PCSX2/pcsx2/master/COPYING.GPLv3"
 rp_module_section="exp"
 rp_module_flags="!all x86"
 
-function depends_pcsx2-qt() {
+function depends_pcsx2-appImage() {
     getDepends jq libfuse2
 }
 
-function install_bin_pcsx2-qt() {
+function install_bin_pcsx2-appImage() {
     mkdir -p "$md_inst/bin"
     version=$(curl --silent -H "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/114.0" "https://api.pcsx2.net/v1/latestReleasesAndPullRequests" | jq -r ".nightlyReleases | .data | .[0] | .version")
     wget --content-disposition https://github.com/PCSX2/pcsx2/releases/download/$version/pcsx2-$version-linux-appimage-x64-Qt.AppImage -O "$md_inst/bin/pcsx2.AppImage"
     chmod +x "$md_inst/bin/pcsx2.AppImage"
 }
 
-function configure_pcsx2-qt() {
+function configure_pcsx2-appImage() {
     mkRomDir "ps2"
     addEmulator 0 "$md_id" "ps2" "$md_inst/bin/pcsx2.AppImage %ROM% -fullscreen -batch"
     addEmulator 1 "$md_id-nogui" "ps2" "$md_inst/bin/pcsx2.AppImage %ROM% -fullscreen -nogui"
